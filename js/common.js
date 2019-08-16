069b060b2aaad815797ccb1a930e279aabffae57
BreakTiger <965066599@qq.com>
2019-08-16 18:08:42 +0800

    tiger 8-16

--- js/common.js
+++ js/common.js
@@ -90,6 +90,19 @@ function emptys() {
 }
 
 // 窗口大小发生变化刷新
-$(window).resize(function () {
-    location.reload()
-});
+// $(window).resize(function () {
+//     location.reload()
+// });
+
+
+//获取地址栏url中的参数
+function GetQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg); //获取url中"?"符后的字符串并正则匹配
+    var context = "";
+    if (r != null)
+        context = r[2];
+    reg = null;
+    r = null;
+    return context == null || context == "" || context == "undefined" ? "" : context;
+}
